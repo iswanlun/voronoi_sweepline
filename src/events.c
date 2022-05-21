900bfac39737302632cb5ac53a6f25739302a10e
iswanlun <iswanlun@asu.edu>
2022-05-21 01:11:32 -0700

    events added

--- src/events.c
+++ src/events.c
@@ -2,8 +2,68 @@
 #include "events.h"
 #include "stddef.h"
 
-void sort_vertex_list( vertex_list* list ) {
+vertex_event* merge_list( vertex_event* left, vertex_event* right ) {
 
+    vertex_event* head;
+
+    if ( left->sweep_y > right->sweep_y ) {
+        head = left;
+        left = left->next;
+    } else {
+        head = right;
+        right = right->next;
+    }
+
+    vertex_event* tail = head;
+
+    while ( left != NULL && right != NULL ) {
+
+        if ( left->sweep_y > right->sweep_y ) {          
+            tail->next = left;
+            tail = left;
+            left = left->next;
+
+        } else {
+            tail->next = right;
+            tail = right;
+            right = right->next;
+        }
+    }
+
+    if ( left == NULL ) {
+        tail->next = right;
+    }
+
+    if ( right == NULL ) {
+        tail->next = left;
+    }
+
+    return head;
+}
+
+vertex_event* sort_vertex_list( vertex_event* head, int length ) {
+
+    if ( head->next = NULL ) {
+        return head;
+
+    } else {
+        vertex_event* left = head;
+        int len = (length/2);
+
+        for (int i = 0; i < len; i++ ) {
+            head = head->next;
+        }
+
+        vertex_event* last = head;
+        vertex_event* right = head->next;
+
+        last->next = NULL;
+
+        left = sort_vertex_list(left, len);
+        right = sort_vertex_list(right, length - len);
+
+        return merge_list( left, right );
+    }
 }
 
 void insert_vertex_event( vertex_list* list, vertex_event** event, float v_x, float v_y, float sweep_y ) {
@@ -12,11 +72,60 @@ void insert_vertex_event( vertex_list* list, vertex_event** event, float v_x, fl
         *event = (vertex_event*) malloc( sizeof(vertex_event) );
         (*event)->next = list->head;
         list->head = (*event);
+        list->head++;
     }
 
     (*event)->sweep_y = sweep_y;
     (*event)->v_site.x = v_x;
     (*event)->v_site.y = v_y;
     
-    sort_vertex_list( list );
+    sort_vertex_list( list->head, list->length );
+}
+
+vertex_event* next_vertex_event( vertex_list* list ) {
+
+    vertex_event* tmp = list->head;
+    list->head = list->head->next;
+    return tmp;
+}
+
+float peek_vertex_event( vertex_list* list ) {
+
+    if ( list->head != NULL ) {
+        return list->head->sweep_y;
+    } else {
+        return 0;
+    }
+}
+
+void null_vertex_event( vertex_list* list, vertex_event** dest ) {
+
+    if ( *dest != NULL ) {
+
+        vertex_event* prev = NULL;
+        vertex_event* index = list->head;
+
+        while ( index != *(dest) ) {
+            prev = index;
+            index = index->next;
+        }
+
+        if (prev != NULL) {
+            prev->next = index->next;
+        }
+
+        free(index);
+        *dest = NULL;
+        list->length--;
+    }
+}
+
+vertex_list* create_vertex_list( void ) {
+
+    vertex_list* list = (vertex_list*) malloc( sizeof(vertex_list) );
+
+    list->head = NULL;
+    list->length = 0;
+
+    return list;
 }
--- src/events.h
+++ src/events.h
@@ -16,13 +16,17 @@ typedef struct _vertex_list {
 
     vertex_event* head;
 
+    int length;
+
 } vertex_list;
 
 void insert_vertex_event( vertex_list* list, vertex_event** event, float v_x, float v_y, float sweep_y );
 
-void next_vertex_event( vertex_list* list, vertex_event* dest );
+float peek_vertex_event( vertex_list* list );
 
-void null_vertex_event( vertex_list* list, vertex_event* dest );
+vertex_event* next_vertex_event( vertex_list* list );
+
+void null_vertex_event( vertex_list* list, vertex_event** dest );
 
 vertex_list* create_vertex_list( void );
 
